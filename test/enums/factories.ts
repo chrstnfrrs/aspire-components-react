bacd6031407b92884ea7f6ed7e5f34e943f11c0a
Christian Farris <christianfarriscs@gmail.com>
2021-03-15 20:32:22 -0500

    Create test enums

---
+++ test/enums/factories.ts
@@ -0,0 +1,10 @@
+export const JUSTIFY_FACTORY = [
+  'start',
+  'end',
+  'center',
+  'around',
+  'between',
+  'evenly',
+];
+
+export const ALIGN_FACTORY = ['start', 'end', 'center', 'stretch', 'baseline'];
