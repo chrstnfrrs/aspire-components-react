59b18e51d37a9ef9cfb57e2aa2ca4010dc39bdb9
Christian Farris <christianfarriscs@gmail.com>
2021-03-17 21:11:00 -0500

    Upgrade to GOML 6.0.0

---
+++ test/unit/utils/test-factory.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import * as RTL from '@testing-library/react';
+
+import { ALLOW_ANY, ITestPropType } from '../../types';
+
+import { testClass } from './test-class';
+
+export const testFactory: ITestPropType = (Component, name, factoryMap) => {
+  let result: ALLOW_ANY;
+
+  describe.each(Object.keys(factoryMap))('When %s prop is used', (prop) => {
+    describe.each(factoryMap[prop])(
+      `When %s is passed into ${prop}`,
+      (value) => {
+        beforeEach(() => {
+          const props = {
+            [prop]: value,
+          };
+
+          result = RTL.render(<Component {...props}>{name}</Component>);
+        });
+        test(`Then the class ${prop}-${value} should be present`, () => {
+          expect(testClass(result, `${prop}-${value}`)).toBe(true);
+        });
+      },
+    );
+  });
+};
