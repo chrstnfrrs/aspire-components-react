ff91bf1e46e838de4175b87e9d0ece57c580778f
Christian Farris <christianfarriscs@gmail.com>
2021-04-26 21:42:28 -0500

    fix keys with .s and /s

--- test/unit/utils/test-factory.tsx
+++ test/unit/utils/test-factory.tsx
@@ -7,6 +7,7 @@ import { testClass } from './test-class';
 
 export const testFactory: ITestPropType = (Component, name, factoryMap) => {
   let result: ALLOW_ANY;
+  let key: string;
 
   describe.each(Object.keys(factoryMap))('When %s prop is used', (prop) => {
     describe.each(factoryMap[prop])(
@@ -17,10 +18,12 @@ export const testFactory: ITestPropType = (Component, name, factoryMap) => {
             [prop]: value,
           };
 
+          key = value.split('.').join('_').split('/').join('__');
+
           result = RTL.render(<Component {...props}>{name}</Component>);
         });
-        test(`Then the class ${prop}-${value} should be present`, () => {
-          expect(testClass(result, `${prop}-${value}`)).toBe(true);
+        test(`Then the class for ${value} should be present`, () => {
+          expect(testClass(result, `${prop}-${key}`)).toBe(true);
         });
       },
     );
