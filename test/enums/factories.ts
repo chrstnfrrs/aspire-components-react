89fc54040a0d611ebdd7f16674be01584f342fe2
Christian Farris <christianfarriscs@gmail.com>
2021-04-26 21:40:33 -0500

    export all factories

--- test/enums/factories.ts
+++ test/enums/factories.ts
@@ -1,12 +1,23 @@
-const ALIGN_FACTORY = ['start', 'end', 'center', 'stretch', 'baseline'];
+import { ALIGN_FACTORY } from './align-factory';
+import { COLOR_FACTORY } from './color-factory';
+import { DIRECTION_FACTORY } from './direction-factory';
+import { DISPLAY_FACTORY } from './display-factory';
+import { HEIGHT_FACTORY } from './height-factory';
+import { JUSTIFY_FACTORY } from './justify-factory';
+import { LAYOUT_FACTORIES } from './layout-factories';
+import { MARGIN_FACTORY } from './margin-factory';
+import { PADDING_FACTORY } from './padding-factory';
+import { WIDTH_FACTORY } from './width-factory';
 
-const JUSTIFY_FACTORY = [
-  'start',
-  'end',
-  'center',
-  'around',
-  'between',
-  'evenly',
-];
-
-export { ALIGN_FACTORY, JUSTIFY_FACTORY };
+export {
+  ALIGN_FACTORY,
+  COLOR_FACTORY,
+  DIRECTION_FACTORY,
+  DISPLAY_FACTORY,
+  HEIGHT_FACTORY,
+  JUSTIFY_FACTORY,
+  LAYOUT_FACTORIES,
+  MARGIN_FACTORY,
+  PADDING_FACTORY,
+  WIDTH_FACTORY,
+};
