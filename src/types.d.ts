efa15ec2f708d0b8b5653a7a73fd9f4997e7e3ae
Christian Farris <christianfarriscs@gmail.com>
2021-04-29 22:12:18 -0500

    create ILayoutComponent interface

---
+++ src/types.d.ts
@@ -0,0 +1,20 @@
+interface ILayoutComponent {
+  align?: string;
+  backgroundColor?: string;
+  className?: string;
+  color?: string;
+  direction?: string;
+  display?: string;
+  height?: string;
+  justify?: string;
+  margin?: string;
+  maxHeight?: string;
+  maxWidth?: string;
+  minHeight?: string;
+  minWidth?: string;
+  padding?: string;
+  style?: React.CSSProperties | undefined;
+  width?: string;
+}
+
+export { ILayoutComponent };
