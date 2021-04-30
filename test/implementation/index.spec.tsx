02c1221eed66feed620ba93fcd277a7898baaf90
Christian Farris <christianfarriscs@gmail.com>
2021-04-30 01:44:19 -0500

    test ASection export

--- test/implementation/index.spec.tsx
+++ test/implementation/index.spec.tsx
@@ -2,17 +2,20 @@ import * as ABoxFile from '../../src/ABox/ABox';
 import * as AColFile from '../../src/ACol/ACol';
 import * as AContainerFile from '../../src/AContainer/AContainer';
 import * as ARowFile from '../../src/ARow/ARow';
+import * as ASectionFile from '../../src/ASection/ASection';
 import * as AspireComponentsReact from '../../src';
 
 jest.mock('../../src/ABox/ABox');
 jest.mock('../../src/ACol/ACol');
 jest.mock('../../src/AContainer/AContainer');
 jest.mock('../../src/ARow/ARow');
+jest.mock('../../src/ASection/ASection');
 
 const { ABox } = ABoxFile as jest.Mocked<typeof ABoxFile>;
 const { ACol } = AColFile as jest.Mocked<typeof AColFile>;
 const { AContainer } = AContainerFile as jest.Mocked<typeof AContainerFile>;
 const { ARow } = ARowFile as jest.Mocked<typeof ARowFile>;
+const { ASection } = ASectionFile as jest.Mocked<typeof ASectionFile>;
 
 describe('Given aspire-components-react', () => {
   test('then the following components should be exported', () => {
@@ -21,6 +24,7 @@ describe('Given aspire-components-react', () => {
       ACol,
       AContainer,
       ARow,
+      ASection,
     });
   });
 });
