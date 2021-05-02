ac718feaf2787d6d563a6a7b58efbeb04b25e48c
Christian Farris <christianfarriscs@gmail.com>
2021-05-02 03:16:28 -0500

    test exporting ANav

--- test/implementation/index.spec.tsx
+++ test/implementation/index.spec.tsx
@@ -1,6 +1,7 @@
 import * as ABoxFile from '../../src/ABox/ABox';
 import * as AColFile from '../../src/ACol/ACol';
 import * as AContainerFile from '../../src/AContainer/AContainer';
+import * as ANavFile from '../../src/ANav/ANav';
 import * as ARowFile from '../../src/ARow/ARow';
 import * as ASectionFile from '../../src/ASection/ASection';
 import * as AspireComponentsReact from '../../src';
@@ -8,12 +9,14 @@ import * as AspireComponentsReact from '../../src';
 jest.mock('../../src/ABox/ABox');
 jest.mock('../../src/ACol/ACol');
 jest.mock('../../src/AContainer/AContainer');
+jest.mock('../../src/ANav/ANav');
 jest.mock('../../src/ARow/ARow');
 jest.mock('../../src/ASection/ASection');
 
 const { ABox } = ABoxFile as jest.Mocked<typeof ABoxFile>;
 const { ACol } = AColFile as jest.Mocked<typeof AColFile>;
 const { AContainer } = AContainerFile as jest.Mocked<typeof AContainerFile>;
+const { ANav } = ANavFile as jest.Mocked<typeof ANavFile>;
 const { ARow } = ARowFile as jest.Mocked<typeof ARowFile>;
 const { ASection } = ASectionFile as jest.Mocked<typeof ASectionFile>;
 
@@ -23,6 +26,7 @@ describe('Given aspire-components-react', () => {
       ABox,
       ACol,
       AContainer,
+      ANav,
       ARow,
       ASection,
     });
