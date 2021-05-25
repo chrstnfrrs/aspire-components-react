09b6e074b2699d16a2aa9693d9aa20faa9dcfce7
Christian Farris <christianfarriscs@gmail.com>
2021-05-24 23:16:38 -0500

    test fontsize

--- test/implementation/ALabel.spec.tsx
+++ test/implementation/ALabel.spec.tsx
@@ -1,5 +1,5 @@
 import { ALabel } from '../../src/ALabel/ALabel';
-import { ELEMENT_ENUM } from '../enums/factories';
+import { ELEMENT_ENUM, FONTSIZE_ENUM } from '../enums/factories';
 
 import { testComponent } from './utils/test-component';
 
@@ -7,6 +7,7 @@ describe('Given ALabel component is used', () => {
   const ALabelProps = {
     factory: {
       ...ELEMENT_ENUM,
+      fontSize: FONTSIZE_ENUM,
     },
   };
 
