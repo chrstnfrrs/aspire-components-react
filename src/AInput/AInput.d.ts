a07a5c6f48c6f581724a6614dde85fbe2602fb11
Christian Farris <christianfarriscs@gmail.com>
2021-05-27 22:03:53 -0500

    Remove extra types

--- src/AInput/AInput.d.ts
+++ src/AInput/AInput.d.ts
@@ -1,14 +1,7 @@
 import { IElementComponent, ILayoutComponent } from '../types';
 
-interface ILabel extends IElementComponent {
-  fontSize?: string;
-  value: string;
-}
-
 interface IAInput extends IElementComponent {
-  box?: ILayoutComponent;
   fontSize?: string;
-  label?: ILabel | string;
   name?: string;
   placeholder?: string;
 }
