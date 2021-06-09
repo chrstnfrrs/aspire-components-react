1d87e2098e961718eaf96426f81addaa2f85dd40
Christian Farris <christianfarriscs@gmail.com>
2021-06-08 23:17:30 -0500

    add onBlur

--- src/ATextArea/ATextArea.d.ts
+++ src/ATextArea/ATextArea.d.ts
@@ -1,7 +1,10 @@
+import * as React from 'react';
+
 import { IElementComponent } from '../types';
 
 interface IATextArea extends IElementComponent {
   fontSize?: string;
   name?: string;
   placeholder?: string;
+  onBlur?: React.FocusEventHandler<HTMLTextAreaElement>;
 }
