942cc7ad137036ffac17384576dd6e5a795cf599
Christian Farris <christianfarriscs@gmail.com>
2021-06-08 23:16:01 -0500

    testing setTheme

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import { ARow } from './ARow/ARow';
 import { ASection } from './ASection/ASection';
 import { ASpacer } from './ASpacer/ASpacer';
 import { ATextArea } from './ATextArea/ATextArea';
+import { setTheme } from './functions/set-theme';
 
 import 'aspire-components-scss/build/index.scss';
 
@@ -28,4 +29,5 @@ export {
   ASection,
   ASpacer,
   ATextArea,
+  setTheme,
 };
