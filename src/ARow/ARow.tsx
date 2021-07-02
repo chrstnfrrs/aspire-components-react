5b3b8339dd1cacd5603ece3a33bf396c5b78b651
Christian Farris <christianfarriscs@gmail.com>
2021-07-02 00:30:10 -0500

    destructure props and utilize data-testid

--- src/ARow/ARow.tsx
+++ src/ARow/ARow.tsx
@@ -5,11 +5,49 @@ import { setLayout } from 'aspire-components-functions';
 import { ILayoutComponent } from '../types';
 import './ARow.scss';
 
-const ARow: React.FC<ILayoutComponent> = (props) => {
-  const classList = [props.className, 'a-row', ...setLayout(props)];
+const ARow: React.FC<ILayoutComponent> = ({
+  align,
+  backgroundColor,
+  className,
+  color,
+  'data-testid': testid,
+  direction,
+  display,
+  height,
+  justify,
+  margin,
+  maxHeight,
+  maxWidth,
+  minHeight,
+  minWidth,
+  padding,
+  style,
+  width,
+  ...props
+}) => {
+  const classList = [
+    className,
+    'a-row',
+    ...setLayout({
+      align,
+      backgroundColor,
+      color,
+      direction,
+      display,
+      height,
+      justify,
+      margin,
+      maxHeight,
+      maxWidth,
+      minHeight,
+      minWidth,
+      padding,
+      width,
+    }),
+  ];
 
   return (
-    <div className={clsx(classList)} style={props.style}>
+    <div className={clsx(classList)} data-testid={testid} style={style}>
       {props.children}
     </div>
   );
