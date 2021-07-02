ad5f5ef7cdb09e539676a49ac25752aa679f8c85
Christian Farris <christianfarriscs@gmail.com>
2021-07-01 22:42:27 -0500

    destructure props for ABox

--- src/ABox/ABox.tsx
+++ src/ABox/ABox.tsx
@@ -5,11 +5,49 @@ import { setLayout } from 'aspire-components-functions';
 import { ILayoutComponent } from '../types.d';
 import './ABox.scss';
 
-const ABox: React.FC<ILayoutComponent> = (props) => {
-  const classList = [props.className, 'a-box', ...setLayout(props)];
+const ABox: React.FC<ILayoutComponent> = ({
+  align,
+  backgroundColor,
+  className,
+  color,
+  'data-testid': testid,
+  direction,
+  display,
+  height,
+  justify,
+  margin,
+  maxHeight,
+  maxWidth,
+  minHeight,
+  minWidth,
+  padding,
+  style,
+  width,
+  ...props
+}) => {
+  const classList = [
+    className,
+    'a-box',
+    ...setLayout({
+      align,
+      backgroundColor,
+      color,
+      direction,
+      display,
+      height,
+      justify,
+      margin,
+      maxHeight,
+      maxWidth,
+      minHeight,
+      minWidth,
+      padding,
+      width,
+    }),
+  ];
 
   return (
-    <div className={clsx(classList)} style={props.style}>
+    <div className={clsx(classList)} data-testid={testid} style={style}>
       {props.children}
     </div>
   );
