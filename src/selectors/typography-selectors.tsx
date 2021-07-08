9292bde28e862c87b9cadd44086b1c16b9766fb4
Christian Farris <christianfarriscs@gmail.com>
2021-07-07 22:59:57 -0500

    move classList setup, elemect selection to typography selectors, simplify HX elements

--- src/selectors/typography-selectors.tsx
+++ src/selectors/typography-selectors.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import clsx from 'clsx';
+import { setFontSize } from 'aspire-components-functions';
 
 import * as Types from '../types.d';
 
@@ -8,64 +9,43 @@ type HeadingAs = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
 interface IStyleProps {
   as?: HeadingAs;
+  className?: string;
+  fontSize?: string;
 }
 
 interface IHeading {
   as?: HeadingAs;
+  className?: string;
+  'data-testid': string;
+  fontSize?: string;
   type?: HeadingType;
 }
 
 type GetElementClass = (as: HeadingAs) => string;
 type GetElementClassList = (styleProps: IStyleProps) => string;
-type GetElementType = (type: HeadingType) => React.FC;
+type GetElementType = (type: HeadingType) => React.FC<IHeading>;
 type GetElement = (
   props: IHeading,
 ) => {
-  Component: React.FC;
+  Component: React.FC<IHeading>;
   className: string;
   props: Types.AllowAny;
 };
 
-const H1: React.FC = (props) => (
-  <h1 data-testid='h1' {...props}>
-    {props.children}
-  </h1>
-);
+const H1: React.FC<IHeading> = (props) => <h1>{props.children}</h1>;
 
-const H2: React.FC = (props) => (
-  <h2 data-testid='h2' {...props}>
-    {props.children}
-  </h2>
-);
+const H2: React.FC<IHeading> = (props) => <h2>{props.children}</h2>;
 
-const H3: React.FC = (props) => (
-  <h3 data-testid='h3' {...props}>
-    {props.children}
-  </h3>
-);
+const H3: React.FC<IHeading> = (props) => <h3>{props.children}</h3>;
 
-const H4: React.FC = (props) => (
-  <h4 data-testid='h4' {...props}>
-    {props.children}
-  </h4>
-);
+const H4: React.FC<IHeading> = (props) => <h4>{props.children}</h4>;
 
-const H5: React.FC = (props) => (
-  <h5 data-testid='h5' {...props}>
-    {props.children}
-  </h5>
-);
+const H5: React.FC<IHeading> = (props) => <h5>{props.children}</h5>;
 
-const H6: React.FC = (props) => (
-  <h6 data-testid='h6' {...props}>
-    {props.children}
-  </h6>
-);
+const H6: React.FC<IHeading> = (props) => <h6>{props.children}</h6>;
 
-const AStrong: React.FC = (props) => (
-  <strong data-testid='strong' {...props}>
-    {props.children}
-  </strong>
+const AStrong: React.FC<IHeading> = (props) => (
+  <strong>{props.children}</strong>
 );
 
 const getElementType: GetElementType = (type) => {
@@ -95,22 +75,40 @@ const getElementClass: GetElementClass = (as) => {
   return map[as];
 };
 
-const getElementClassList: GetElementClassList = (props) => {
-  const classes = [getElementClass(props.as as HeadingAs)];
+const getElementClassList: GetElementClassList = ({
+  as,
+  className,
+  fontSize,
+}) => {
+  const classes = [
+    className,
+    getElementClass(as as HeadingAs),
+    setFontSize(fontSize),
+  ];
 
   return clsx(classes);
 };
 
-const getElement: GetElement = ({ as, type, ...props }) => {
+const getElement: GetElement = ({
+  as,
+  className,
+  fontSize,
+  type,
+  ...props
+}) => {
   const Component = getElementType(type as HeadingType);
-  const className = getElementClassList({ as });
+  const classes = getElementClassList({
+    as,
+    className,
+    fontSize,
+  });
 
   return {
-    className,
+    className: classes,
     Component,
     props,
   };
 };
 
-export type { HeadingType, IHeading };
 export { getElement };
+export type { HeadingType, IHeading };
