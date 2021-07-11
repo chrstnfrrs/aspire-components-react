42f8027e52abeb77ec63a2e44297a031e8d28b7a
Christian Farris <christianfarriscs@gmail.com>
2021-07-11 00:51:09 -0500

    implementation test AH5

---
+++ test/implementation/AH5.spec.tsx
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import * as RTL from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Chance from 'chance';
+
+import { AH5 } from '../../src/AH5/AH5';
+import * as TypographySelectors from '../../src/typography/heading-selectors';
+
+jest.mock('../../src/typography/heading-selectors');
+
+const chance = new Chance();
+const { getElement } = TypographySelectors as jest.Mocked<
+  typeof TypographySelectors
+>;
+
+describe('<AH5 />', () => {
+  let text: string, testid: string;
+
+  beforeEach(() => {
+    text = chance.string();
+    testid = chance.string();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    getElement.mockReturnValue({
+      className: chance.string(),
+      // eslint-disable-next-line react/display-name
+      Component: (props) => (
+        <p data-testid={props['data-testid']}>{props.children}</p>
+      ),
+    });
+  });
+
+  afterEach(jest.resetAllMocks);
+  describe('easy path', () => {
+    beforeEach(() => {
+      RTL.render(<AH5 data-testid={testid}>{text}</AH5>);
+    });
+    test('should get element', () => {
+      expect(getElement).toHaveBeenCalledTimes(1);
+      expect(getElement).toHaveBeenCalledWith({
+        as: 'h5',
+        children: text,
+        'data-testid': testid,
+        type: 'h5',
+      });
+    });
+    test('should be able to find by children', () => {
+      expect(RTL.screen.getByText(text)).toBeVisible();
+    });
+    test('should be able to find by testid', () => {
+      expect(RTL.screen.getByTestId(testid)).toBeVisible();
+    });
+  });
+  describe('when as is passed in props', () => {
+    let as: TypographySelectors.HeadingAs;
+
+    beforeEach(() => {
+      as = chance.pickone(['h1', 'h2', 'h3', 'h4', 'h6']);
+      RTL.render(
+        <AH5 as={as} data-testid={testid}>
+          {text}
+        </AH5>,
+      );
+    });
+    test('should get element', () => {
+      expect(getElement).toHaveBeenCalledTimes(1);
+      expect(getElement).toHaveBeenCalledWith({
+        as,
+        children: text,
+        'data-testid': testid,
+        type: 'h5',
+      });
+    });
+    test('should be able to find by children', () => {
+      expect(RTL.screen.getByText(text)).toBeVisible();
+    });
+    test('should be able to find by testid', () => {
+      expect(RTL.screen.getByTestId(testid)).toBeVisible();
+    });
+  });
+  describe('when type is passed in props', () => {
+    let type: TypographySelectors.HeadingType;
+
+    beforeEach(() => {
+      type = chance.pickone(['h1', 'h2', 'h3', 'h4', 'h6', 'strong']);
+      RTL.render(
+        <AH5 data-testid={testid} type={type}>
+          {text}
+        </AH5>,
+      );
+    });
+    test('should get element', () => {
+      expect(getElement).toHaveBeenCalledTimes(1);
+      expect(getElement).toHaveBeenCalledWith({
+        as: 'h5',
+        children: text,
+        'data-testid': testid,
+        type,
+      });
+    });
+    test('should be able to find by children', () => {
+      expect(RTL.screen.getByText(text)).toBeVisible();
+    });
+    test('should be able to find by testid', () => {
+      expect(RTL.screen.getByTestId(testid)).toBeVisible();
+    });
+  });
+});
