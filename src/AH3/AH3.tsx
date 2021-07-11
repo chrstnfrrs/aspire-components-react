b7a6b4dc62c5c424c6be1629043c25b4a420f493
Christian Farris <christianfarriscs@gmail.com>
2021-07-11 00:29:29 -0500

    Create AH3

---
+++ src/AH3/AH3.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import * as TypographySelectors from '../typography/heading-selectors';
+import '../typography/headings.scss';
+
+const AH3: React.FC<TypographySelectors.IHeading> = ({
+  as,
+  'data-testid': testid,
+  type,
+  ...props
+}) => {
+  const componentProps = {
+    ...props,
+    as: as || 'h3',
+    'data-testid': testid,
+    type: type || 'h3',
+  };
+
+  const { Component, className } = TypographySelectors.getElement(
+    componentProps,
+  );
+
+  return (
+    <Component className={className} data-testid={testid}>
+      {props.children}
+    </Component>
+  );
+};
+
+export { AH3 };
